3625511a2a26acf3d7b663d98c30dc1c7adb069c
s4na <appletea.umauma@gmail.com>
2019-09-18 16:20:42 +0900

    TODOリスト機能追加

--- main.js
+++ main.js
@@ -1,12 +1,60 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'vue_todo_list'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index + 1
+    })
+    todoStorage.uid = todos.length + 1
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
 
 const app = new Vue({
   el: '#app',
 
   data: {
+    todos: []
+  },
 
+  created() {
+    this.todos = todoStorage.fetch()
+  },
+
+  watch: {
+    todos: {
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      deep: true
+    }
   },
 
   methods: {
-
+    addTodo: function() {
+      var body = this.$refs.body
+      if (!body.value.length) {
+        return
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        body: body.value,
+        date: Date.now(),
+      })
+      body.value = ''
+    },
+    deleteTodo: function (item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    },
+    dateFormat: function (date) {
+      return moment(date).format("YYYY/MM/DD HH:mm")
+    }
   }
 })
